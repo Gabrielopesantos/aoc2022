d0f3d088251b049571c44e2be42d7966503e77a8
Gabriel Santos <gabrielopesantos97@gmail.com>
2022-12-10 15:59:32 +0000

    day 10 python

---
+++ day10/part2.py
@@ -0,0 +1,223 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+OPP_CYCLES = {
+    "addx": 2,
+    "noop": 1,
+}
+
+crt = ["." for _ in range(6 * 40)]
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    x = 1
+    total_cycles = 0
+    for line in lines:
+        opp, *rest = line.split()
+        opp_val = int(rest[0]) if rest else 0
+        opp_cycles = OPP_CYCLES[opp]
+
+        while opp_cycles:
+            opp_cycles -= 1
+            row_pos = total_cycles
+            if total_cycles > 39:
+                row_pos = (total_cycles % 40)
+            if x == row_pos:
+                crt[total_cycles] = "#"
+            elif x - 1 == row_pos:
+                crt[total_cycles] = "#"
+            elif x + 1 == row_pos:
+                crt[total_cycles] = "#"
+
+            total_cycles += 1
+        x += opp_val
+
+    for end in range(40, 40*7, 40):
+        init = end - 40
+        print("".join(crt[init:end]))
+    return None
+
+
+INPUT_S = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+EXPECTED = 13140
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
